18b9ac2a8d6247606458cf2149590d0dbed4ee8c
KevinPetrishin <kevin.petrishin0@gmail.com>
2022-12-24 14:46:54 -0500

    added a few more strings

--- strings2.py
+++ strings2.py
@@ -1,10 +1,21 @@
-#
+#                   1
 #         012345678901234
 parrot = "Norwegian Blue"
 
-print(parrot[0:6])  # Norweg
+print(parrot[0:6])      # Norweg
+print(parrot[-14:8])
 
 print(parrot[-4:-2])    # Bl
-print(parrot[-4:12])    # Bl
+print(parrot[-4:12])
+
+print(parrot[0:6:2])    # Nre
+print(parrot[0:6:3])    # Nw
+
+number = "9,223;372:036 854,775;807"
+seperators = (number[1::4])
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
 
 
