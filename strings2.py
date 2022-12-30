4d5e5d8d11b01954368fc77d4a9c6bd5109988b8
KevinPetrishin <kevin.petrishin0@gmail.com>
2022-12-29 20:34:17 -0500

    error fix

--- strings2.py
+++ strings2.py
@@ -15,7 +15,6 @@ number = "9,223;372:036 854,775;807"
 seperators = (number[1::4])
 print(seperators)
 
-values = "".join(char if char not in seperators else " " for char in number).split()
+values = "".join(
+    char if char not in seperators else " " for char in number).split()
 print([int(val) for val in values])
-
-
