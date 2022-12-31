b77fb9ed1e58c0f828e0a538cea3883a352762b6
KevinPetrishin <kevin.petrishin0@gmail.com>
2022-12-31 18:51:12 -0500

    fixed strings in 'sliceback'

--- sliceback.py
+++ sliceback.py
@@ -8,5 +8,9 @@ print(qpo)
 edcba = letters[4::-1]          # edcba
 print(edcba)
 
-backwards = letters[25:17:-1]   # zyxwvuts
+backwards = letters[:-9:-1]   # zyxwvuts
 print(backwards)
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
