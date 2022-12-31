276eb5221c0650a85ae291a708e6b4349b1f6e0f
KevinPetrishin <kevin.petrishin0@gmail.com>
2022-12-31 14:53:55 -0500

    slicing backwards

---
+++ sliceback.py
@@ -0,0 +1,12 @@
+#                    1          2
+#          01234567890123456789012345
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+qpo = letters[16:13:-1]         # qpo
+print(qpo)
+
+edcba = letters[4::-1]          # edcba
+print(edcba)
+
+backwards = letters[25:17:-1]   # zyxwvuts
+print(backwards)
