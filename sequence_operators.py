4a0493eab242ad6345e186d7fe6047cd32aee5ef
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-01 16:55:43 -0500

    more string operators

--- sequence_operators.py
+++ sequence_operators.py
@@ -6,6 +6,15 @@ string5 = "fjords "
 
 print(string1 + string2 + string3 + string4 + string5)
 
-print("he's " "probably " "pining " "for the " "jfords")
+print("he's " "probably " "pining " "for the " "fjords")
 
 print("Hello " * 5)
+
+print("Hello " * (5 + 4))
+print("Hello " * 5 + "4")
+
+today = "friday"
+print("day" in today)       # True
+print("fri" in today)       # True
+print("thur" in today)      # False
+print("parrot" in "fjord")  # False
