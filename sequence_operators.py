4c072a55393766954dfe64f42602df3c897d01a5
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-01 03:54:40 -0500

    added 'sequence_operators'

---
+++ sequence_operators.py
@@ -0,0 +1,11 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords "
+
+print(string1 + string2 + string3 + string4 + string5)
+
+print("he's " "probably " "pining " "for the " "jfords")
+
+print("Hello " * 5)
