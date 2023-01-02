e6957ec3241f5c543960e4b675630cfd361ff9e9
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-02 00:52:51 -0500

    added some f'strings

--- strings.py
+++ strings.py
@@ -18,4 +18,9 @@ print(type(greeting))
 print(type(age))
 
 age_in_words = "2 years"
+print(name + f" is {age} years old")
 print(type(age))
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
