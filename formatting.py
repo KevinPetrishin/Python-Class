6647a6980769fc115c9768727e9c28dc39984dfd
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-02 00:48:10 -0500

    trying to figure out how formatting works

---
+++ formatting.py
@@ -0,0 +1,23 @@
+for i in range(1, 13):
+    print("No. {0:2} square is {1:3} and cube is {2:4}".format(
+        i, i ** 2, i ** 3))
+
+print()
+
+for i in range(1, 13):
+    print("No. {0:2} square is {1:<3} and cube is {2:^4}".format(
+        i, i ** 2, i ** 3))
+
+print()
+
+print("Pi is approximately {0:12}".format(22 / 7))
+print("Pi is approximately {0:12f}".format(22 / 7))
+print("Pi is approximately {0:12.50f}".format(22 / 7))
+print("Pi is approximately {0:52.50f}".format(22 / 7))
+print("Pi is approximately {0:62.50f}".format(22 / 7))
+print("Pi is approximately {0:<72.50f}".format(22 / 7))
+print("Pi is approximately {0:<72.54f}".format(22 / 7))
+
+for i in range(1, 13):
+    print("No. {} square is {} and cube is {:4}".format(
+        i, i ** 2, i ** 3))
