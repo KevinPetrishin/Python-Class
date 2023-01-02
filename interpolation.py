c46009e22d80d6cf47f5e2078fd18c6634cc1009
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-02 01:00:29 -0500

    DO NOT USE THIS CODE IN 'PYTHON 3'

---
+++ interpolation.py
@@ -0,0 +1,9 @@
+# do not use this code in python 3 !!
+age = 17
+print("My age is %d years" % age)
+
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s" % (age, major, 6, minor))
+print("Pi is approximately %f % (22 / 7)")
+print("Pi is approximately %60.50f" % (22 / 7))
