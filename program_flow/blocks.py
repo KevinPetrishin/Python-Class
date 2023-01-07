d034d62e33e69285eeafa2af55d5f5c5cf5a04ba
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-07 17:10:37 -0500

    created a simple questionnaire asking name and age, calculating if you are old enough to vote

--- program_flow/blocks.py
+++ program_flow/blocks.py
@@ -1,3 +1,17 @@
-for i in range(1, 13):
-    print("No. {} sqared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
-    print("*" * 80)
+name = input("Please enter your name: ")
+age = int(input("How old are you, {0}? ".format(name)))
+print(age)
+
+if age >= 18:
+    print("You are old enough to vote")
+    print("Please put an X in the box")
+else:
+    print("Please come back in {0} years".format(18 - age))
+
+# or
+
+if age < 18:
+    print("Please come back in {0} years".format(18 - age))
+else:
+    print("You are old enough to vote")
+    print("Please put an X in the box")
