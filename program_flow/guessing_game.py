f9769710c23cdf21d6255a5c7285a50b3810d650
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-10 18:46:23 -0500

    improved the guessing game by adding if's and else statements whenthe second guess is incorrect

--- program_flow/guessing_game.py
+++ program_flow/guessing_game.py
@@ -5,7 +5,17 @@ guess = int(input())
 
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())
+    if guess == answer:
+        print("Welcome done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
 elif guess > answer:
     print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
 else:
     print("You got it first time")
