b645765d89111b192a1f9708f2abdbcf0b2885d7
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-11 16:17:47 -0500

    changed for the program to check if the code is right first and go from there instead of checking if the code is wrong first

--- program_flow/guessing_game.py
+++ program_flow/guessing_game.py
@@ -3,7 +3,9 @@ answer = 5
 print("Please guess a number between 1 and 10")
 guess = int(input())
 
-if guess != answer:
+if guess == answer:
+    print("You got it first time")
+else:
     if guess < answer:
         print("Please guess higher")
     else:   # guess must be greater than answer
@@ -13,14 +15,12 @@ if guess != answer:
         print("Well done, you guessed it")
     else:
         print("Sorry, you have not guessed correctly")
-else:
-    print("You got it first time")
 
 # if guess < answer:
 #     print("Please guess higher")
 #     guess = int(input())
 #     if guess == answer:
-#         print("Welcome done, you guessed it")
+#         print("Well done, you guessed it")
 #     else:
 #         print("Sorry, you have not guessed correctly")
 # elif guess > answer:
