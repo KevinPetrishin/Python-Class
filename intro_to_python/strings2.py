7b7abfe1505166b42934dd30e1ca4363eb0335d0
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-19 19:28:09 -0500

    created some easy for loops

--- intro_to_python/strings2.py
+++ intro_to_python/strings2.py
@@ -1,20 +1,12 @@
-#                   1
-#         012345678901234
-parrot = "Norwegian Blue"
-
-print(parrot[0:6])      # Norweg
-print(parrot[-14:8])
-
-print(parrot[-4:-2])    # Bl
-print(parrot[-4:12])
-
-print(parrot[0:6:2])    # Nre
-print(parrot[0:6:3])    # Nw
-
 number = "9,223;372:036 854,775;807"
-seperators = (number[1::4])
-print(seperators)
+separators = ("")
+
+for char in number:
+    if not char.isnumeric():
+        separators = separators + char
+
+print(separators)
 
 values = "".join(
-    char if char not in seperators else " " for char in number).split()
+    char if char not in separators else " " for char in number).split()
 print([int(val) for val in values])
