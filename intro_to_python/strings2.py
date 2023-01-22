1ec9fd21091a9d095aab72aebf39da23820cdf6c
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-22 12:19:30 -0500

    experimenting with ranges and for-loops

--- intro_to_python/strings2.py
+++ intro_to_python/strings2.py
@@ -5,8 +5,8 @@ for char in number:
     if not char.isnumeric():
         separators = separators + char
 
-print(separators)
+# print(separators)
 
 values = "".join(
     char if char not in separators else " " for char in number).split()
-print([int(val) for val in values])
+print(sum([int(val) for val in values]))
--- program_flow/condition.py
+++ program_flow/condition.py
@@ -1,7 +1,8 @@
 age = int(input("How old are you? "))
 
 # if age >= 16 and age <= 65:
-if 16 <= age <= 65:
+# if 16 <= age <= 65:
+if age in range(16, 66):
     print("Have a good day at work")
 else:
     print("Enjoy your free time")
