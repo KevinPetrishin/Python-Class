056cdedddc79f1e4a9d8a7e29537e3e044d9decd
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-01-28 19:24:40 -0500

    more updates and improvements for the file

--- program_flow/searching.py
+++ program_flow/searching.py
@@ -4,9 +4,15 @@ item_to_find = "spam"
 found_at = None
 
 # for index in range(6):
-for index in range(len(shopping_list)):
-    if shopping_list[index] == item_to_find:
-        found_at = index
-        break
+# for index in range(len(shopping_list)):
+#     if shopping_list[index] == item_to_find:
+#         found_at = index
+#         break
 
-print("item found at position {}".format(found_at))
+if item_to_find in shopping_list:
+    found_at = shopping_list.index(item_to_find)
+
+if found_at is not None:
+    print("item found at position {}".format(found_at))
+else:
+    print("{} not found".format(found_at))
