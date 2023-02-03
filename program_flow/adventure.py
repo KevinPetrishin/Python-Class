3ac2fd4cff8a50e6af400c2482ccdc4f44751c4e
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-02-03 14:54:53 -0500

    fixed a language mistake

--- program_flow/adventure.py
+++ program_flow/adventure.py
@@ -4,4 +4,4 @@ chosen_exit = ""
 while chosen_exit not in available_exits:
     chosen_exit = input("Please choose a direction: ")
 
-print("Aren't upi glad you got out of there")
+print("Aren't you glad you got out of there")
