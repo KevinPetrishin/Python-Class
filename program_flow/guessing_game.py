8afb92f3161df2a98d3785ee0c8917aa3c58b537
KevinPetrishin <kevin.petrishin0@gmail.com>
2023-02-04 23:49:07 -0500

    more while-loops

--- program_flow/guessing_game.py
+++ program_flow/guessing_game.py
@@ -1,34 +1,18 @@
-answer = 5
+import random
 
-print("Please guess a number between 1 and 10")
-guess = int(input())
+highest = 10
+answer = random.randint(1, highest)
+guess = 0   # initialize to any number that doesn't equal the answer
+print("Please guess a number between 1 and {}: ".format(highest))
 
-if guess == answer:
-    print("You got it first time")
-else:
-    if guess < answer:
-        print("Please guess higher")
-    else:   # guess must be greater than answer
-        print("Please guess lower")
+while guess != answer:
     guess = int(input())
+
     if guess == answer:
         print("Well done, you guessed it")
+        break
     else:
-        print("Sorry, you have not guessed correctly")
-
-# if guess < answer:
-#     print("Please guess higher")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# elif guess > answer:
-#     print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# else:
-#     print("You got it first time")
+        if guess < answer:
+            print("Please guess higher")
+        else:   # guess must be greater than answer
+            print("Please guess lower")
